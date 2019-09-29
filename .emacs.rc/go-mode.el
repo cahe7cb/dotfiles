9441bb898135e6c3ea06cd34da82f32bc6736037
cahe7cb <cahe7cb@users.noreply.github.com>
2019-09-29 19:37:15 -0300

    go-mode/lsp-mode/flycheck setup

--- .emacs.rc/go-mode.el
+++ .emacs.rc/go-mode.el
@@ -1,3 +1,15 @@
 (use-package go-mode)
+
 (add-hook 'go-mode-hook #'flycheck-mode)
 (add-hook 'go-mode-hook #'lsp)
+
+(add-hook 'lsp-after-initialize-hook (lambda ()
+				(when (eq major-mode 'go-mode)
+				  (flycheck-add-next-checker 'go-golint 'lsp-ui))))
+
+(add-hook 'flycheck-mode-hook (lambda ()
+				(when (eq major-mode 'go-mode)
+				  (setq flycheck-checker 'go-golint))))
+
+(setq gofmt-command "goimports")
+(add-hook 'before-save-hook 'gofmt-before-save)
